58dc64f5fb80342d05b67eef624c94f0807d121c
Abebawushibru <abeaba64@gmail.com>
2024-09-23 23:23:40 +0300

    after clearing data

--- src/configf/config.js
+++ src/configf/config.js
@@ -1,3 +1,3 @@
-const ABC_BACKEND_API_URL= "http://localhost:5000"// "https://alenbezacharity.onrender.com" // ;
+const ABC_BACKEND_API_URL= "https://alenbezacharity.onrender.com"// "https://alenbezacharity.onrender.com" // ;
 const ABC_FRONTEND_BASE_URL= "https://alenbezacharity.netlify.app" //"http://localhost:3000"; 
 export {ABC_BACKEND_API_URL,ABC_FRONTEND_BASE_URL}
